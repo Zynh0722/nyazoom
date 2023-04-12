47a51c5c1591f8fc8943f9437e033107e36c3e1e
Zynh0722 <Zynh0722@gmail.com>
2023-04-12 04:59:43 -0700

    caching!!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use async_zip::tokio::write::ZipFileWriter;
 use async_zip::{Compression, ZipEntryBuilder};
 
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::routing::post;
 use axum::{
@@ -9,6 +10,7 @@ use axum::{
     Router,
 };
 
+use chrono::{DateTime, Utc};
 use futures::TryStreamExt;
 
 use rand::distributions::{Alphanumeric, DistString};
@@ -16,10 +18,15 @@ use rand::rngs::SmallRng;
 use rand::SeedableRng;
 
 use sanitize_filename_reader_friendly::sanitize;
+use serde::{Deserialize, Serialize};
+use tokio::io::AsyncReadExt;
+use tokio::sync::Mutex;
 
+use std::collections::HashMap;
 use std::io;
 use std::net::SocketAddr;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use tokio_util::compat::FuturesAsyncWriteCompatExt;
 use tokio_util::io::StreamReader;
@@ -28,8 +35,51 @@ use tower_http::{limit::RequestBodyLimitLayer, services::ServeDir, trace::TraceL
 
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+#[allow(dead_code)]
+#[derive(Debug, Serialize, Deserialize)]
+struct UploadRecord {
+    uploaded: DateTime<Utc>,
+    file: PathBuf,
+    downloads: u8,
+    max_downloads: u8,
+}
+
+impl UploadRecord {
+    fn new(file: PathBuf) -> Self {
+        Self {
+            file,
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for UploadRecord {
+    fn default() -> Self {
+        Self {
+            uploaded: Utc::now(),
+            file: Path::new("").to_owned(),
+            downloads: 0,
+            max_downloads: 5,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct AppState {
+    records: Arc<Mutex<HashMap<String, UploadRecord>>>,
+}
+
+impl AppState {
+    fn new(records: HashMap<String, UploadRecord>) -> Self {
+        Self {
+            records: Arc::new(Mutex::new(records)),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    // Set up logging
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -38,9 +88,10 @@ async fn main() -> io::Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    // uses create_dir_all to create both .cache and .temp inside it in one go
-    make_dir(".cache/.temp").await?;
+    // uses create_dir_all to create both .cache and serve inside it in one go
     make_dir(".cache/serve").await?;
+
+    let state = fetch_cache().await;
 
     // Router Setup
     let with_big_body = Router::new()
@@ -48,7 +99,8 @@ async fn main() -> io::Result<()> {
         .layer(DefaultBodyLimit::disable())
         .layer(RequestBodyLimitLayer::new(
             10 * 1024 * 1024 * 1024, // 10GiB
-        ));
+        ))
+        .with_state(state);
 
     let base = Router::new()
         .nest_service("/", ServeDir::new("dist"))
@@ -70,13 +122,19 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
-async fn upload_to_zip(mut body: Multipart) -> Result<Redirect, (StatusCode, String)> {
+async fn upload_to_zip(
+    State(state): State<AppState>,
+    mut body: Multipart,
+) -> Result<Redirect, (StatusCode, String)> {
+    tracing::debug!("{:?}", *state.records.lock().await);
+
     let cache_name = get_random_name(10);
 
     let archive_path = Path::new(".cache/serve").join(&format!("{}.zip", &cache_name));
+
     tracing::debug!("Zipping: {:?}", &archive_path);
 
-    let mut archive = tokio::fs::File::create(archive_path)
+    let mut archive = tokio::fs::File::create(&archive_path)
         .await
         .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
     let mut writer = ZipFileWriter::new(&mut archive);
@@ -91,8 +149,7 @@ async fn upload_to_zip(mut body: Multipart) -> Result<Redirect, (StatusCode, Str
 
         let stream = field;
         let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
-        let body_reader = StreamReader::new(body_with_io_error);
-        futures::pin_mut!(body_reader);
+        let mut body_reader = StreamReader::new(body_with_io_error);
 
         let builder = ZipEntryBuilder::new(file_name, Compression::Deflate);
         let mut entry_writer = writer
@@ -111,6 +168,13 @@ async fn upload_to_zip(mut body: Multipart) -> Result<Redirect, (StatusCode, Str
             .await
             .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
     }
+
+    let mut records = state.records.lock().await;
+    records.insert(cache_name.clone(), UploadRecord::new(archive_path));
+
+    write_to_cache(&records)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
 
     writer.close().await.unwrap();
 
@@ -164,4 +228,35 @@ pub mod error {
     pub fn io_other(s: &str) -> Error {
         Error::new(ErrorKind::Other, s)
     }
+}
+
+async fn write_to_cache<T, Y>(records: &HashMap<T, Y>) -> io::Result<()>
+where
+    T: Serialize,
+    Y: Serialize,
+{
+    let mut records_cache = tokio::fs::File::create(".cache/data").await.unwrap();
+
+    let mut buf: Vec<u8> = Vec::with_capacity(200);
+    bincode::serialize_into(&mut buf, &*records)
+        .map_err(|err| error::io_other(&err.to_string()))?;
+
+    let bytes_written = tokio::io::copy(&mut buf.as_slice(), &mut records_cache).await?;
+
+    tracing::debug!("state cache size: {}", bytes_written);
+
+    Ok(())
+}
+
+async fn fetch_cache() -> AppState {
+    let records = if let Ok(file) = tokio::fs::File::open(".cache/data").await.as_mut() {
+        let mut buf: Vec<u8> = Vec::with_capacity(200);
+        file.read_to_end(&mut buf).await.unwrap();
+
+        bincode::deserialize_from(&mut buf.as_slice()).unwrap()
+    } else {
+        HashMap::new()
+    };
+
+    AppState::new(records)
 }
